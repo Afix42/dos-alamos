609cce2adb702744406bdeb54b38071b83b85ffe
BrandonLeiva <brandon96leiva96@gmail.com>
2023-06-08 16:36:12 -0400

    Vistas y urls agregadas

--- dosAlamos/core/urls.py
+++ dosAlamos/core/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, Servicios, Registro, contacto, clinica
 
 
 urlpatterns = [
-    path('', home, name='index'),
+    path('', home, name='home'),
+    path('Servicios/',Servicios,name="Servicios"),
+    path('Registro/',Registro,name="Registro"),
+    path('contacto/',contacto,name="contacto"),
+    path('clinica/',clinica,name="clinica"),
 ]
--- dosAlamos/core/views.py
+++ dosAlamos/core/views.py
@@ -4,5 +4,19 @@ from django.shortcuts import render
 
 
 def home(request):
+    return render(request, 'core/Home.html')
+
+def Servicios(request):
+    return render(request, 'core/service.html')
+
+def Registro(request):
+    return render(request, 'core/Registrarse.html')
+
+def Doctores(request):
+    return render(request, 'core/doctores.html')
+
+def contacto(request):
     return render(request, 'core/contacto.html')
-# Create your views here.
+
+def clinica(request):
+    return render(request, 'core/clinict.html')
